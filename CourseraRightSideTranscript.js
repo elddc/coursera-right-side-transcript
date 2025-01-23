18a31325306580d3b5f975c89a625f5573c18289
Emma Chen <78119615+elddc@users.noreply.github.com>
2025-01-23 16:53:53 -0600

    Update transcript positioning

--- CourseraRightSideTranscript.js
+++ CourseraRightSideTranscript.js
@@ -1,13 +1,22 @@
 // ==UserScript==
 // @name         Coursera Right-Side Transcript
 // @namespace    elddc
-// @version      1.0
+// @version      1.1
 // @description  Place the transcript on the right side of the screen.
 // @author       Elddc
 // @match        https://www.coursera.org/learn/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=coursera.org
 // @grant        none
 // ==/UserScript==
+
+/* add auto-scroll; can remove if desired without changing transcript positioning */
+const autoScroll = () => {
+    console.log("scrolling");
+    document.querySelector(".active").scrollIntoView({behavior: "smooth"});
+}
+setInterval(autoScroll, 15000);
+setTimeout(() => document.addEventListener("click", autoScroll), 2000);
+setTimeout(autoScroll, 2000);
 
 /* reposition transcript */
 const styles = document.createElement('style');
@@ -16,15 +25,15 @@ styles.innerHTML = `
   position: fixed;
   left: 0;
   width: 60vw;
-  top: 24vh;
+  top: 20vh;
 }
 #video-player-row > div {
   height: 100%;
 }
-#video-player-row + div {
+#video-player-row + div + div {
   position: relative;
-  left: 58vw;
-  width: 40vw;
+  left: 56vw;
+  width: 36vw;
 }
 div[data-testid="tabs-root"] {
   position: relative;
@@ -35,6 +44,11 @@ div[data-testid="silent-select-field"] {
   position: fixed;
   top: calc(25vh + 400px);
   left: 65px;
+}
+
+/* only needed for auto-scrolling */
+.active {
+  scroll-margin-top: 200px;
 }
 `;
 document.head.appendChild(styles);
